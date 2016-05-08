80e3ec013725df65deed23df6c93c1ab6e649e28
Nikola <nikola@lucky-kat.com>
2016-05-08 14:15:25 +0200

    first try

--- src/windows/vungleproxy.js
+++ src/windows/vungleproxy.js
@@ -1,10 +1,41 @@
-cordova.commandProxy.add("Echo", {
-    echo: function (successCallback, errorCallback, strInput) {
-        if (!strInput || !strInput.length) {
-            errorCallback("Error, something was wrong with the input string. =>" + strInput);
+cordova.commandProxy.add("VunglePlugin", {
+    /**
+     * Pass app ID to Vungle. Should only be called once.
+     * @param {Function} successCallback - callback when function succeeds. Not used here.
+     * @param {Function} errorCallback - callback when function fails.
+     * @param {String} appID - Vungle Application ID, found in the Vungle Dashboard
+     */
+    setUp: function (successCallback, errorCallback, appId) {
+    	if (typeof appId !== 'string') {
+    		errorCallback("appId should be of type String.");
+    		return;
+    	}
+        VungleComponent.Vungle.setUp(appId);
+    },
+    adPlayable: function (successCallback, errorCallback) {
+        var res = VungleComponent.Vungle.adPlayable;
+        console.log(res);
+        if (res)
+            successCallback(res);
+        else
+            errorCallback("no ad yet playable, adPlayable === " + res);
+        return res;
+    },
+    playAd: function (successCallback, errorCallback) {
+        VungleComponent.Vungle.playAd();
+    }
+});
+
+/*
+cordova.commandProxy.add("EchoPlugin",{
+    echo:function(successCallback, errorCallback, strInput) {
+        var res = EchoRuntimeComponent.EchoPluginRT.echo(strInput);
+        if(res.indexOf("Error") == 0) {
+            errorCallback(res);
         }
         else {
-            successCallback(strInput);
+            successCallback(res);
         }
     }
 });
+*/
--- www/vungle.js
+++ www/vungle.js
@@ -1,1 +1,39 @@
-var Vungle
+var Vungle = {
+
+};
+
+window.Vungle.setUp = function (appId, callback) {
+    cordova.exec(
+        callback,
+        function (errMsg) {
+            console.log("ERROR:", errMsg);
+        },
+        "VunglePlugin",
+        "setUp",
+        [appId]
+    );
+};
+
+window.Vungle.adPlayable = function (callback) {
+    cordova.exec(
+        callback,
+        function (errMsg) {
+            console.log("ERROR:", errMsg);
+        },
+        "VunglePlugin",
+        "adPlayable",
+        []
+    );
+};
+
+window.Vungle.playAd = function (callback) {
+    cordova.exec(
+        callback,
+        function (errMsg) {
+            console.log("ERROR:", errMsg);
+        },
+        "VunglePlugin",
+        "playAd",
+        []
+    );
+};
